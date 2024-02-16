9facc160605e84f001422e72ede7d470d2d2cd43
Sidney Carvalho Pacheco <115200807+SidneyCarv90@users.noreply.github.com>
2024-02-16 19:07:07 +0000

    Adicionado agina inicial

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home(){
+    return(
+        <div>
+            <h1>Para o amor da minha vida!</h1>
+            <p>Queria dizer que te amo mais do que tudo nessa vida. Você é tudo pra mim...</p>
+        </div>
+    )
+};
+
+export default Home;
